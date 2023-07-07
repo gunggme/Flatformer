dbf86a45c8ce35fae9d9439eb5f4cb30f9498b12
roor48 <kmgoio@naver.com>
2023-07-07 16:10:12 +0900

    asdf

--- Assets/Scripts/Managers/NameManager.cs
+++ Assets/Scripts/Managers/NameManager.cs
@@ -18,6 +18,7 @@ public class NameManager : MonoBehaviour
     [SerializeField] private GameObject rankImage;
 
     [SerializeField] private TMP_Text[] ranksTexts;
+    [SerializeField] private int maxRanks;
 
     private bool isClick = false;
     
@@ -70,33 +71,38 @@ public class NameManager : MonoBehaviour
         }
         StartCoroutine(SetRanks());
     }
+
     private IEnumerator SetRanks()
     {
         UnityWebRequest request = UnityWebRequest.Get("pcs.pah.kr:1005/api/ranking");
         yield return request.SendWebRequest();
 
         string rawData = request.error == null ? request.downloadHandler.text : "error";
+        
+        if(rawData=="error")
+            Debug.Log(request.downloadHandler.error);
 
         List<RankSet> rankDatas = new();
         string slicedData = "";
         // 쓸데없는 정보들 다 버리고 content 안에 있는 내용만 추출
-        for (int i = 12; rawData[i-1] != ']' && rankDatas.Count < 4; i++) // 마지막 요소이거나, 상위 4명을 다 뽑았을 때 break
+        for (int i = 12; rawData[i-1] != ']' && rankDatas.Count < maxRanks; i++) // 마지막 요소이거나, maxRanks명 만큼 뽑았을 때 break
         {
             slicedData += rawData[i];
             if (rawData[i] == '}') // '}'이 나왔다는 것은 데이터가 끝났다는 뜻
             {
                 rankDatas.Add(JsonUtility.FromJson<RankSet>(slicedData));
                 slicedData = "";
-                i++; // '}'뒤에는 ','가 있으니 1을 더해준다
+                i++;                        // '}'뒤에는 ','가 있으니 1을 더해준다
                 
                 /*
-                 for문 조건식 i-1한 이유
-                 마지막에 i++이 실행되면 ']'을 건너뛰기 때문에
+                 for문 조건식 i-1한 이유 :
+                 마지막요소는 '}'뒤에 ','가 없기 때문에
+                 ']'를 건너 뜀
                 */
             }
         }
 
-        for (int i = 0; i < 4; i++) // 상위 4위까지
+        for (int i = 0; i < maxRanks; i++)
         {
             TimeSpan t = TimeSpan.FromSeconds(rankDatas[i].time);
             ranksTexts[i].text = $"Rank {i+1}. {rankDatas[i].playerName}\ntime : {t.Hours:D2}:{t.Minutes:D2}:{t.Seconds:D2}";
